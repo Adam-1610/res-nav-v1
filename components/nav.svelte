5b29bf7fbd3991d5368c710c636f14ddaff27725
Adam-1610 <adamhameed234@gmail.com>
2024-08-08 22:37:52 +0100

    Add files via upload

---
+++ components/nav.svelte
@@ -0,0 +1,9 @@
+<link rel="stylesheet" href="/src/lib/stylesheets/nav.css">
+
+<nav class="[ navbar ]">
+    <ul class="[ navbar-nav ]">
+        <li>
+            Logo
+        </li>
+    </ul>
+</nav>
