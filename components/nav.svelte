49ca3aeff04a78e217a2e6c966fea9fcd7058d52
Adam Hameed^C <adamhameed234@gmail.com^C>
2024-08-12 10:23:21 +0100

    Changed the Svelte file to Html

--- components/nav.svelte
+++
@@ -1,9 +0,0 @@
-<link rel="stylesheet" href="/src/lib/stylesheets/nav.css">
-
-<nav class="[ navbar ]">
-    <ul class="[ navbar-nav ]">
-        <li>
-            Logo
-        </li>
-    </ul>
-</nav>
